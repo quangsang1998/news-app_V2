583739d121d76ad56b53366cb7153ead985d6164
Hoàng Bình Dương <duonghb.coltech@gmail.com>
2021-06-14 10:37:57 +0700

    refector HistoryFragment

--- app/src/main/java/com/duonghb/testbitrise/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/duonghb/testbitrise/ui/history/HistoryFragment.kt
@@ -1,10 +1,7 @@
 package com.duonghb.testbitrise.ui.history
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -12,63 +9,28 @@ import com.duonghb.testbitrise.R
 import com.duonghb.testbitrise.databinding.HistoryFragmentBinding
 import com.duonghb.testbitrise.ui.home.HomeFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.history_fragment.*
 
 @AndroidEntryPoint
-class HistoryFragment : Fragment() {
+class HistoryFragment : Fragment(R.layout.history_fragment) {
 
-    private val historyFragment = R.layout.history_fragment
-
-    private lateinit var binding: HistoryFragmentBinding
-
-    private val historyViewModel: HistoryViewModel by viewModels()
-
-    private val historyAdapter by lazy {
-        HistoryAdapter(
-            clickHistoryItemCallback = {
-                findNavController().navigate(
-                    HomeFragmentDirections.actionNavigationHomeToNavigationNewsDetail(
-                        it.url
-                    )
-                )
-            }
-        )
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        super.onCreateView(inflater, container, savedInstanceState)
-        binding = DataBindingUtil.inflate(inflater, historyFragment, container, false)
-        binding.lifecycleOwner = viewLifecycleOwner
-        return binding.root
-    }
+    private val viewModel: HistoryViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        init()
-        initUi()
-        registerLivedataListeners()
-    }
+        val binding = HistoryFragmentBinding.bind(view)
+        binding.lifecycleOwner = viewLifecycleOwner
 
-    private fun init() {
-        historyViewModel.getNewsHistoryListDatabase()
-    }
+        val adapter = HistoryAdapter {
+            findNavController().navigate(
+                HomeFragmentDirections.actionNavigationHomeToNavigationNewsDetail(it.url)
+            )
+        }
+        binding.historyRecyclerView.adapter = adapter
 
-    private fun initUi() {
-        historyRecyclerView.adapter = historyAdapter
-    }
-
-    private fun registerLivedataListeners() {
-
-        historyViewModel.loadNewsHistoryListDatabaseCompleted.observe(
-            viewLifecycleOwner,
-            {
-                historyAdapter.setHistoryItems(it)
-            }
-        )
+        viewModel.getNewsHistoryListDatabase()
+        viewModel.loadNewsHistoryListDatabaseCompleted.observe(viewLifecycleOwner) {
+            adapter.setHistoryItems(it)
+        }
     }
 
     companion object {
